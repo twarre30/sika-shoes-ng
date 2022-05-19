9b4d54d415bcfeafcf4fa3565ea9502b900a3bd2
Tiffany Warren <twarre30@gmail.com>
2022-05-19 16:12:39 -0500

    update routing file

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, ROUTES, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
-
+const routing: Routes = [
+  { path: '/home', component: HomePageComponent },
+  { path: '/about', component: AboutPageComponent },
+  { path: '/products', component: ProductsPageComponent },
+  { path: '/products/:id', component: ProductPageComponent },
+  {path: '/', redirectTo: '/home', pathMatch:'full'},
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(
+      routing,
+      { enableTracing: true}
+    ),
+    
+    CommonModule,
+
   ]
 })
 export class RoutingModule { }
